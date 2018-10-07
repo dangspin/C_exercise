585f7a8f50d7befb519b185e081519301090a7b6
Charlie Dang <dangxiaoqian@gmail.com>
2018-10-07 17:49:01 +0800

    Update ex-1-3.c

--- Ch1/ex-1-3.c
+++ Ch1/ex-1-3.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+/*Exercise 1-3 Modify the temperature conversion program to print a heading above the table.*/
 /*Print Fahrenheit-Celsius table for fahr = 0, 20, ..., 300*/
 
 int main()
