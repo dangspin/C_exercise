ad80e624e2a9ea7d6f61e33bac1d4dcbd168cc75
Charlie Dang <dangxiaoqian@gmail.com>
2018-10-08 20:36:21 +0800

    Create ex-1-10.c

---
+++ Ch1/ex-1-10.c
@@ -0,0 +1,2 @@
+/*Write a program to copy its input to its output, replacing each tab by \t
+  each backsapce by \b, and each backsalsh by \\. This makes tabs and backspaces visible in an unambiguous way.*/
