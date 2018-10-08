96170d48ae077e381a490aa903709ca71bb2e42d
Charlie Dang <dangxiaoqian@gmail.com>
2018-10-08 16:41:14 +0800

    Update ex-1-6.c

--- Ch1/ex-1-6.c
+++ Ch1/ex-1-6.c
@@ -1,3 +1,21 @@
 /*Verify that the expression getchar() != EOF is 0 or 1*/
 
 #include <stdio.h>
+
+
+int main()
+{
+    int eof;
+    char c;
+    
+    c = getchar();
+    while (eof = (c!=EOF))
+    {
+        printf("EOF is %d\n",eof);
+        printf("The char is %c\n\n", c);
+        c = getchar();
+    }
+    
+    printf("%d\n", eof);
+    return 0;
+}
