2749a2d4d697208e6b9b27fb1d97e4d46fdd197a
Charlie Dang <dangxiaoqian@gmail.com>
2018-10-08 16:07:45 +0800

    Update ex-1-5.c

--- Ch1/ex-1-5.c
+++ Ch1/ex-1-5.c
@@ -1,3 +1,21 @@
 /*Modify the temperature conversion program to print the table in reverse order, that is, from 300 degrees to 0*/
 
 #include <stdio.h>
+
+#define lower 0
+#define upper 300
+#define step  20
+
+int main()
+{
+    float celsius, fahr;
+    
+    printf("  F\tC\n\n");
+    
+    for (fahr = upper; fahr >= lower; fahr = fahr - step)
+    {
+        printf("%3.0f\t%6.1f\n",fahr, (5.0/9.0)*(fahr - 32));
+    }
+    
+    return 0;
+}
