a600c0b479f278359d7413706b3f16ca0f15aac4
Charlie Dang <dangxiaoqian@gmail.com>
2018-10-08 16:00:50 +0800

    Update ex-1-4.c

--- Ch1/ex-1-4.c
+++ Ch1/ex-1-4.c
@@ -1,3 +1,23 @@
 /*Write a program to print the corresponding Celsius to Fahrenheit table*/
 
 #include <stdio.h>
+
+int main()
+{
+    float celsius, fahr;
+    int lower, upper, step;
+    
+    lower = 0;
+    upper = 300;
+    step = 20;
+    
+    printf("  C\tF\n\n");
+    
+    celsius = lower;
+    while (celsius <= upper)
+    {
+        printf("%3.0f\t%6.1f\n",celsius, (9.0/5.0)*celsius+32);
+        celsius = celsius + step;
+    }
+    return 0;
+}
