bcb7532c416135cce3704422b372d467339003b6
Charlie Dang <dangxiaoqian@gmail.com>
2018-10-09 23:15:24 +0800

    Update ex-1-8.c

--- Ch1/ex-1-8.c
+++ Ch1/ex-1-8.c
@@ -1,3 +1,33 @@
 /*Write a program to count blanks, tabs, and newlines.*/
 
 #include <stdio.h>
+
+int main() {
+	char c;
+	int nn, nt, nb;
+	nn = nt = nb = 0;
+	
+	while ((c = getchar())!=EOF)
+	{
+		if (c == ' ')
+		{
+			++nb;
+		}
+		if (c == '\t')
+		{
+			++nt;
+		}
+		if (c == '\n')
+		{
+			++nn;
+		}
+	}
+	
+	 printf("------------------\n");
+   printf("Total spaces:\t%d\n", nb);
+   printf("Total tabs:\t%d\n", nt);
+   printf("Total lines:\t%d\n", nn);
+	
+	return 0;
+}
+
