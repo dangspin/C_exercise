68bafa5a442e342cfc1cd0cb79323fc66805a6d2
Charlie Dang <dangxiaoqian@gmail.com>
2018-10-13 10:49:46 +0800

    Update ex-1-9.c

--- Ch1/ex-1-9.c
+++ Ch1/ex-1-9.c
@@ -1,3 +1,31 @@
 /*Write a program to copy its input to its output, replacing each string of one or more blanks by a single blank*/
 
 #include <stdio.h>
+
+int main() {
+	int c; /*we use int type to store the character*/
+	
+	/*Initial the first c*/
+	c = getchar();
+	
+	while (c != EOF)
+	{
+		putchar(c);
+		
+		/*if c indeed is a blank, we then check when it will be terminated.*/
+		if (c == ' ')
+		{
+			while (c == ' ')
+			{
+				c = getchar();
+			}
+			putchar(c);
+		}
+		
+		c = getchar();
+	}
+	return 0;
+}
+
+
+
