cc72e1daf302d7852e075d8a3e015b7ecaf2dd36
Charlie Dang <dangxiaoqian@gmail.com>
2018-10-13 19:41:36 +0800

    Update ex-1-13.c

--- Ch1/ex-1-13.c
+++ Ch1/ex-1-13.c
@@ -2,3 +2,45 @@
 horizontal; a vertical orientation is more challenging.*/
 
 #include <stdio.h>
+
+int main()
+{
+	int c; /*Input character*/
+	int count;   /*count how many characters are in one word*/
+	
+	int distribution[11]; /*Distribution array*/
+	
+	/*Initialization of the array*/
+	for (int i = 0; i< 11; i++){
+		distribution[i] = 0;
+	}
+	
+	/*Core algorithm*/
+	c = getchar();
+	while (c != EOF){
+		if (c!=' ' && c!='\t' && c!='\n'){
+			++count;
+		} else if (count >0){
+			if (count <= 10) {
+				++distribution[count-1];
+			} else {
+				++distribution[10];
+			}
+			count = 0;
+		}
+		
+		c = getchar();
+	}
+	printf("%d\n", distribution[10]);
+	
+	for (int i = 0; i<=10;i++) {
+		printf("[%d]:",i+1);
+		while (distribution[i] > 0){
+			printf("x ");
+			distribution[i]--;
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}
